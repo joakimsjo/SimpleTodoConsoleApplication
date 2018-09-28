67e46452184f810ca14a4562adada6ee82e9abaf
Joakim Sjøhaug <joakim.sjohaug@gmail.com>
2018-09-28 13:32:52 +0200

    Added convert class for deserilization from JSON

--- application/ToDoElement.cs
+++ application/ToDoElement.cs
@@ -1,19 +1,29 @@
 using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
+namespace application
+{
 
-namespace application {
-
-    public class ToDoElement : IToDoElement
+    public class TodoElement : ITodoElement
     {
         private readonly string formatString = "#{0} {1}";
         public int _id;
         public string _description;
         public bool _done;
 
-        public ToDoElement(int id, string description) {
+        public TodoElement(int id, string description)
+        {
             _id = id;
             _description = description;
             _done = false;
+        }
+
+        public TodoElement(int id, string description, bool done)
+        {
+            _id = id;
+            _description = description;
+            _done = done;
         }
 
         public void MarkAsDone()
@@ -21,7 +31,8 @@ namespace application {
             _done = true;
         }
 
-        public bool IsDone() {
+        public bool IsDone()
+        {
             return _done;
         }
         public string GetId()
@@ -36,6 +47,30 @@ namespace application {
         public override string ToString()
         {
             return string.Format(formatString, GetId(), GetDescription());
+        }
+    }
+
+    public class TodoElementConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(ITodoElement));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject TodoObject = JObject.Load(reader);
+            return new TodoElement(TodoObject["_id"].Value<int>(), TodoObject["_description"].Value<string>(), TodoObject["_done"].Value<bool>());
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
         }
     }
 }
